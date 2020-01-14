5950baf316cc1d670d2c47cda2d904344fa5f05b
Matthew J. Hughes <mjhughes707@gmail.com>
2020-01-14 12:36:04 -0800

    updated the results component to show total correct guesses

--- src/components/DisplayResults.js
+++ src/components/DisplayResults.js
@@ -1,12 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
-import resultsList from "../resultsList";
 
-const DisplayResults = () => {
+const DisplayResults = ({ userAnswers, timeRemaining }) => {
   return (
     <div className="state-container">
-      {resultsList.map((state, index) => (
+      {timeRemaining === 0 && <h3>You guessed {userAnswers.length} states!</h3>}
+      {userAnswers.map((state, index) => (
         <div key={index} className="state-item">
           <FontAwesomeIcon icon={faCheckCircle} className="state-check" />
           <p className="state-name">{state}</p>
@@ -14,6 +15,11 @@ const DisplayResults = () => {
       ))}
     </div>
   );
+};
+
+DisplayResults.propTypes = {
+  userAnswers: PropTypes.array.isRequired,
+  timeRemaining: PropTypes.number.isRequired,
 };
 
 export default DisplayResults;
