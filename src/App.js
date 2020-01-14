da79249f12dd28eaa71c7fec0e2cd74919efd02b
Matthew J. Hughes <mjhughes707@gmail.com>
2020-01-14 12:36:49 -0800

    cleaned up the App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import React, { Fragment, useState } from "react";
 import Header from "./components/Header";
 import UserForm from "./components/UserForm";
+import StartClock from "./components/StartClock";
 import DisplayResults from "./components/DisplayResults";
+import QuizLogic from "./components/QuizLogic";
+import resultsList from "./resultsList";
 
 const App = () => {
+  const { timeRemaining, isTimeRunning, startGame, textBoxRef } = QuizLogic();
   const [userAnswers, setUserAnswers] = useState([]);
 
   const getAnswer = answer => {
-    const newAnswers = [answer, ...userAnswers];
-    setUserAnswers(newAnswers);
-    console.log(userAnswers);
+    if (resultsList.includes(answer) && !userAnswers.includes(answer)) {
+      const newAnswers = [answer, ...userAnswers];
+      setUserAnswers(newAnswers);
+    } else if (userAnswers.includes(answer)) {
+      alert(
+        `${answer.toUpperCase()} has already been guessed! Please try again!`
+      );
+    } else if (!resultsList.includes(answer)) {
+      alert(`${answer.toUpperCase()} is not a U.S. state. Please try again!`);
+    }
   };
 
   return (
     <Fragment>
       <Header />
       <main className="container">
-        <UserForm getAnswer={getAnswer} />
-        <DisplayResults />
+        <UserForm
+          getAnswer={getAnswer}
+          textBoxRef={textBoxRef}
+          isTimeRunning={isTimeRunning}
+        />
+        <StartClock
+          timeRemaining={timeRemaining}
+          startGame={startGame}
+          isTimeRunning={isTimeRunning}
+        />
+        <DisplayResults
+          userAnswers={userAnswers}
+          timeRemaining={timeRemaining}
+        />
       </main>
     </Fragment>
   );
