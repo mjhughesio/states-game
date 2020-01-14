38fd7230b02ab425b345a9603010c6fdc64a1ffa
Matthew J. Hughes <mjhughes707@gmail.com>
2020-01-14 12:34:11 -0800

    created a component to hold most of the logic for the game

---
+++ src/components/QuizLogic.js
@@ -0,0 +1,37 @@
+import { useState, useRef, useEffect } from "react";
+
+const QuizLogic = (startingTime = 10) => {
+  const [timeRemaining, setTimeRemaining] = useState(startingTime);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const textBoxRef = useRef(null);
+
+  const startGame = () => {
+    setIsTimeRunning(true);
+    setTimeRemaining(startingTime);
+    textBoxRef.current.disabled = false;
+    textBoxRef.current.focus();
+  };
+
+  const endGame = () => {
+    setIsTimeRunning(false);
+  };
+
+  useEffect(() => {
+    if (isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining(time => time - 1);
+      }, 1000);
+    } else if (timeRemaining === 0) {
+      endGame();
+    }
+  }, [timeRemaining, isTimeRunning]);
+
+  return {
+    timeRemaining,
+    isTimeRunning,
+    startGame,
+    textBoxRef,
+  };
+};
+
+export default QuizLogic;
