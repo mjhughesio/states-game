dc25da9ad7d8e9c825809e39e3cab3ae6826ba0a
Matthew J. Hughes <mjhughes707@gmail.com>
2020-01-15 19:31:48 -0800

    updated dynamic messaging

--- src/components/TotalStates.js
+++ src/components/TotalStates.js
@@ -1,41 +1,47 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const TotalStates = ({ timeRemaining, userAnswers }) => {
+const TotalStates = ({ isTimeRunning, timeRemaining, userAnswers }) => {
   let motivation;
   let color;
-  if (userAnswers.length === 0) {
+  if (!isTimeRunning && userAnswers.length === 0) {
     motivation = "Are you ready to get started?";
-  } else if (userAnswers.length < 10) {
+  } else if (isTimeRunning && userAnswers.length === 0) {
+    motivation = "Here we go! Good luck!";
+  } else if (isTimeRunning && userAnswers.length < 10) {
     motivation = "Great start! Keep going!";
-  } else if (userAnswers.length < 20) {
+  } else if (isTimeRunning && userAnswers.length < 20) {
     motivation = "Nice! Double digits!";
-  } else if (userAnswers.length < 30) {
+  } else if (isTimeRunning && userAnswers.length < 30) {
     motivation = "In the 20's now! Well done!";
-  } else if (userAnswers.length < 40) {
+  } else if (isTimeRunning && userAnswers.length < 40) {
     motivation = "The dirty 30's! Keep going!";
-  } else if (userAnswers.length < 45) {
+  } else if (isTimeRunning && userAnswers.length < 45) {
     motivation = "Over the hill! Almost there!";
-  } else if (userAnswers.length < 50) {
+  } else if (isTimeRunning && userAnswers.length < 50) {
     motivation = "So close! We believe!";
-  } else if (userAnswers.length === 50) {
+  } else if (isTimeRunning && userAnswers.length === 50) {
     motivation = "CONGRATULATIONS!";
     color = "green";
   }
 
+  let tracker;
+  if (timeRemaining === 0) {
+    tracker = `Time's up! That's ${userAnswers.length} states!`;
+  } else {
+    tracker = `You've guessed ${userAnswers.length} / 50 states!`;
+  }
+
   return (
     <div className="total-container">
-      {timeRemaining === 0 ? (
-        <h3>Time's up! That's {userAnswers.length} states!</h3>
-      ) : (
-        <h3>You've guessed {userAnswers.length} / 50 states!</h3>
-      )}
+      <h3>{tracker}</h3>
       <p style={{ color }}>{motivation}</p>
     </div>
   );
 };
 
 TotalStates.propTypes = {
+  isTimeRunning: PropTypes.bool.isRequired,
   timeRemaining: PropTypes.number.isRequired,
   userAnswers: PropTypes.array.isRequired,
 };
