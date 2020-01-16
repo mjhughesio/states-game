cf039af0b45f1bc02bd195e683247890d0486e66
Matthew J. Hughes <mjhughes707@gmail.com>
2020-01-15 22:36:39 -0800

    Updates

---
+++ precache-manifest.04b69013426664fa45a123253451da9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a33ad5aedf8b7168b4edfba35b5d6b31",
+    "url": "/states-game/index.html"
+  },
+  {
+    "revision": "b0922250d9708ddbeb41",
+    "url": "/states-game/static/css/main.9c282f9d.chunk.css"
+  },
+  {
+    "revision": "bcfe82aebb08cabed8ff",
+    "url": "/states-game/static/js/2.45d06c13.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/states-game/static/js/2.45d06c13.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b0922250d9708ddbeb41",
+    "url": "/states-game/static/js/main.77a1fcd9.chunk.js"
+  },
+  {
+    "revision": "53a8562ad83b26825717",
+    "url": "/states-game/static/js/runtime-main.e6b141ed.js"
+  }
+]);
