51c9ca35e11322b97b4e289c8f416cf602be0e75
Matthew J. Hughes <mjhughes707@gmail.com>
2020-01-15 19:31:22 -0800

    moved userAnswers state into QuizLogic to allow for reset

--- src/components/QuizLogic.js
+++ src/components/QuizLogic.js
@@ -1,11 +1,13 @@
 import { useState, useRef, useEffect } from "react";
 
 const QuizLogic = (startingTime = 360) => {
+  const [userAnswers, setUserAnswers] = useState([]);
   const [timeRemaining, setTimeRemaining] = useState(startingTime);
   const [isTimeRunning, setIsTimeRunning] = useState(false);
   const textBoxRef = useRef(null);
 
   const startGame = () => {
+    setUserAnswers([]);
     setIsTimeRunning(true);
     setTimeRemaining(startingTime);
     textBoxRef.current.disabled = false;
@@ -27,6 +29,8 @@ const QuizLogic = (startingTime = 360) => {
   }, [timeRemaining, isTimeRunning]);
 
   return {
+    userAnswers,
+    setUserAnswers,
     timeRemaining,
     isTimeRunning,
     startGame,
